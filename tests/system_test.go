7fb5d6440a47a61fd90f7ca8d95da21f8f60b9aa
Chris Campbell <chrs.cmpbl@gmail.com>
2017-06-14 20:08:58 -0600

    fix tests to be more consistent

--- tests/system_test.go
+++ tests/system_test.go
@@ -2,6 +2,7 @@ package system
 
 import (
 	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/url"
 	"testing"
@@ -19,28 +20,36 @@ type Score struct {
 func TestVote(t *testing.T) {
 
 	time.Sleep(time.Second * 5)
-	// create a channel to send results
-	// resultChan := make(chan Score)
 
-	// connect to the result events
+	//
+	//	Listen for Results
+	//
+
+	resultChan := make(chan Score)
 	c, err := gosocketio.Dial(
 		gosocketio.GetUrl("result", 80, false),
 		transport.GetDefaultWebsocketTransport(),
 	)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	c.On(gosocketio.OnConnection, func(c *gosocketio.Channel, args interface{}) {
 		t.Log("connected")
 		c.Join("scores")
 	})
-	c.On("scores", func(c *gosocketio.Channel, args interface{}) {
-		t.Log(args)
-		//resultChan <- score
+	c.On("scores", func(c *gosocketio.Channel, args string) {
+		var score Score
+		if err := json.Unmarshal([]byte(args), &score); err != nil {
+			t.Fatal(err)
+		}
+		resultChan <- score
 	})
 	defer c.Close()
 
-	// vote for a category
+	//
+	// Vote for A
+	//
+
 	postData := url.Values{}
 	postData.Add("vote", "a")
 
@@ -53,11 +62,12 @@ func TestVote(t *testing.T) {
 		t.Error("got non OK status", resp.StatusCode)
 	}
 
-	time.Sleep(time.Second * 10)
+	//
+	//	Validate result
+	//
 
-	t.Error("error")
-	// score := <-resultChan
-	// if score.A != 1 {
-	// 	t.Error("score was not expected value", score.A)
-	// }
+	score := <-resultChan
+	if score.A < 1 {
+		t.Error("score was not expected value", score.A)
+	}
 }
